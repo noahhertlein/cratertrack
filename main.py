305880b819a738df5869c13838863190c01c4cbb
nwagjason <jasonhemingway@me.com>
2025-04-01 21:51:12 -0500

    changes

--- main.py
+++ main.py
@@ -39,4 +39,4 @@ def serve(path):
     return send_file('static/index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
